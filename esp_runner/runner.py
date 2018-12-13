a6d8cd8c7cd1ed640f92d45a13a69476f190959f
Aaron Craelius <aaron@regen.network>
2018-12-13 17:22:05 -0500

    Switching to python based implementation because that's what the data scientists use

---
+++ esp_runner/runner.py
@@ -0,0 +1,7 @@
+ # pull from postges job queue
+ # sleep if no jobs
+
+# def get_job():
+
+# def main():
+
---
+++ sql/V1_0__init.sql
@@ -0,0 +1,15 @@
+CREATE TABLE job
+(
+  id bigserial,
+  done boolean default false,
+  outstanding_dependencies int,
+  submitted_at timestamptz,
+  runner_id text,
+  runner_started_at timestamptz
+);
+
+CREATE TABLE job_dependency
+(
+  job_id bigserial references job,
+  dep_id bigserial references job
+);
