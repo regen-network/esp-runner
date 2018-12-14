f08b71f53deaa1f56318a90d649584d7680a746d
Aaron Craelius <aaron@regen.network>
2018-12-14 15:36:38 -0500

    WIP on pg job runner

--- sql/V1_0__init.sql
+++ sql/V1_0__init.sql
@@ -1,8 +1,12 @@
 CREATE TABLE job
 (
-  id bigserial,
+  id text,
+  git_url text,
+  input jsonb,
   done boolean default false,
-  outstanding_dependencies int,
+  deps jsonb,
+  pending_deps int,
+  output_url text,
   submitted_at timestamptz,
   runner_id text,
   runner_started_at timestamptz
@@ -10,10 +14,26 @@ CREATE TABLE job
 
 CREATE TABLE job_dependency
 (
-  job_id bigserial references job,
-  dep_id bigserial references job
+  job_id text references job,
+  dep_id text references job
 );
 
 -- See https://dba.stackexchange.com/a/69497
 CREATE FUNCTION job_get_one(runner_id text) AS $$
 $$ LANGUAGE SQL VOLATILE;
+
+CREATE FUNCTION job_report_completion(id text, output_url text) AS $$
+  BEGIN
+    UPDATE job
+    SET done = true, output_url = job_report_completion.output_url
+    WHERE id = job_report_completion.id;
+    UPDATE job SET pending_deps = pending_deps - 1
+    WHERE id IN (SELECT job_id FROM job_dependency WHERE dep_id = job_report_completion.id);
+  end;
+$$ LANGUAGE plpgsql VOLATILE;
+
+CREATE TABLE docker_image
+(
+  git_url text not null primary key,
+  image_url text
+);
